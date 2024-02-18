702b52a18a9c8a95a4d1aed8e6bc5add1e1d53ed
fabblack1993 <ndougafabienne77@gmail.com>
2024-02-18 18:58:19 +0100

    mon depot sur git

---
+++ question.js
@@ -0,0 +1,59 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Qui est le président du Cameroun?",
+    answer: "B. Paul Biya",
+    options: [
+      "A. Maurice Kamto",
+      "B. Paul Biya",
+      "C. Cabral Libii",
+      "D. Joshua Osih",
+    ]
+  },
+  {
+    numb: 2,
+    question: "Premier président du Cameroun?",
+    answer: "C. Amadou Ahidjo",
+    options: [
+      "A. André-Marie Mbida",
+      "B. Paul Biya",
+      "C. Amadou Ahidjo",
+      "D. John Fru Ndi",
+    ]
+  },
+  
+  {
+    numb: 3,
+    question: "Capitale du Cameroun?",
+    answer: "B. Yaoundé",
+    options: [
+      "A. Bafoussam",
+      "B. Yaoundé",
+      "C. Ebolowa",
+      "D. Douala",
+    ]
+  },
+  {
+    numb: 4,
+    question: "Quelle est la devise du Cameroun?",
+    answer: "B. Paix-Travail-Patrie",
+    options: [
+      "A. Silence on travaille",
+      "B. Paix-Travail-Patrie",
+      "C. Honneur et Fidélité",
+      "D. Fraternité-Courage-Patriotisme",
+    ]
+  },
+  {
+    numb: 5,
+    question: "Qui est le president de la Fecafoot?",
+    answer: "D. Samuel Eto'o Fils",
+    options: [
+      "A. Iya Mohamed",
+      "B. Moussa Njoya",
+      "C. Franck Happi",
+      "D. Samuel Eto'o Fils",
+    ]
+  },
+  
+];
