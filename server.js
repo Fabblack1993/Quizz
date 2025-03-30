1dd8ece4deafdc1c1c480f246d26b0957bc53b62
Ndouga Fabienne <ndougafabienne77@.com>
2025-03-30 06:39:30 +0100

    Ajout des fichiers du projet et configuration finale

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 // Middleware pour servir les fichiers statiques (CSS, images)
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 // ROUTE PRINCIPALE POUR SERVIR LES PAGES HTML
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/about', (req, res) => {
